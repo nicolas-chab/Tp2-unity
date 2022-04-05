bacbfafe1ea0be7f8ac8dd34a4a62391344c526a
nicolas-chab <101563616+nicolas-chab@users.noreply.github.com>
2022-04-05 15:53:45 -0300

    mayoria de edad

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+     public int edadUsuario;
+
+    void Start()
+       
+    {
+       
+
+        if (edadUsuario >= 18) 
+        {
+            Debug.Log("es mayor de edad");
+        }
+        else 
+        {
+            Debug.Log("no es mayor de edad");
+        }
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
