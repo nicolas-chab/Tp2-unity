ecd9ddf1b827646105b4baf89678a9ccb2986df9
nicolas-chab <47203227@est.ort.edu.ar>
2022-04-04 23:06:28 -0300

    trabajo terminado 0.1
    esta version no valida el texto puesto en el cartel

--- Assets/Scripts/Validarcontraseña.cs
+++ Assets/Scripts/Validarcontraseña.cs
@@ -1,23 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Validarcontraseña : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    string contraseñaCorrecta;
+    public Text ingresoUsuario;
+    string contraseñaUsuario;
+    public GameObject cartelitoMsj;
+    public Text textoMsj;
+
     void Start()
     {
-        
-    }
+        contraseñaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+    } 
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
-    public void validarcontraseña()
+
+    public void ValidarContraseña()
     {
+        contraseñaUsuario = ingresoUsuario.text;
 
+        if (contraseñaUsuario == contraseñaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "bienvenido";
+            Debug.Log("Bienvenido");
+            
+        }
+
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
     }
-}
 
+}
